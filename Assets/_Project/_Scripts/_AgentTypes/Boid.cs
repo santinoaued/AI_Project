7f5ea553878a2fb4822311227b9729f9ee202979
Santino Aued <santino.aued@gmail.com>
2026-09-10 20:33:47 -0300

    I finished the flocking behaviors and assigned a weight to each one.

--- Assets/_Project/_Scripts/_AgentTypes/Boid.cs
+++ Assets/_Project/_Scripts/_AgentTypes/Boid.cs
@@ -2,12 +2,18 @@ using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Boid : Agent
 {
     [SerializeField] private float _separationRadius;
     [SerializeField] private float _neighborRadius;
     [SerializeField] private LayerMask _boidLayer;
+
+    // weight
+    [SerializeField] private float _separationWeight = 2f;
+    [SerializeField] private float _alignmentWeight = 1f;
+    [SerializeField] private float _cohesionWeight = 1f;
 
     private List<Boid> GetNeighbors(float radius)
     {
@@ -38,7 +44,6 @@ public class Boid : Agent
         if (neighbors.Count > 0) 
         {
             separationForce /= neighbors.Count;
-          
         }
 
         return CalculateSteering(separationForce);
@@ -47,14 +52,49 @@ public class Boid : Agent
     private Vector3 Alignment() 
     {
         List<Boid> neighbors = GetNeighbors(_neighborRadius);
-        return Vector3.zero;
+        Vector3 alignmentForce = Vector3.zero;
+
+        foreach (Boid neighbor in neighbors) 
+        {
+            alignmentForce += neighbor.Velocity;
+        }
+
+        if (neighbors.Count > 0)
+        {
+            alignmentForce /= neighbors.Count;
+        }
+
+        return CalculateSteering(alignmentForce);
     }
-    void Start()
+
+    private Vector3 Cohesion()
     {
-        
+        List<Boid> neighbors = GetNeighbors(_neighborRadius);
+        Vector3 cohesionForce = Vector3.zero;
+
+        foreach (Boid neighbor in neighbors)
+        {
+            cohesionForce += neighbor.transform.position;
+        }
+
+        if (neighbors.Count > 0)
+        {
+            cohesionForce /= neighbors.Count;
+        }
+
+        cohesionForce -= transform.position;
+
+        return CalculateSteering(cohesionForce);
     }
+
+    private Vector3 CalculateFlocking()
+    {
+        return (Separation() * _separationWeight) + (Alignment() * _alignmentWeight) + (Cohesion() * _cohesionWeight);
+    }
+
     void Update()
     {
-        
+        Vector3 steering = CalculateFlocking();
+        Move(steering);
     }
 }
